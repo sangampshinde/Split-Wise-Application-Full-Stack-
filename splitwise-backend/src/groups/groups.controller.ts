4ef9ecc802aa6af390e81b733f2daf1a8263e8c6
spsspit <sangam.shinde@spitsolutions.com>
2025-09-06 10:16:26 +0530

    group module

--- splitwise-backend/src/groups/groups.controller.ts
+++ splitwise-backend/src/groups/groups.controller.ts
@@ -1,34 +1,26 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { GroupsService } from './groups.service';
 import { CreateGroupDto } from './dto/create-group.dto';
 import { UpdateGroupDto } from './dto/update-group.dto';
+import { JwtAuthGuard } from 'src/auth/jwt.auth.guard';
+ import { User } from '../common/decorators/user.decorator';
 
 @Controller('groups')
+@UseGuards(JwtAuthGuard)
 export class GroupsController {
   constructor(private readonly groupsService: GroupsService) {}
 
-  @Post()
-  create(@Body() createGroupDto: CreateGroupDto) {
-    return this.groupsService.create(createGroupDto);
-  }
 
-  @Get()
-  findAll() {
-    return this.groupsService.findAll();
+  @Post()
+  async create(@Body() createGroupDto: CreateGroupDto,@User() user: any){
+    return this.groupsService.create(createGroupDto,user.id)
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.groupsService.findOne(+id);
+  async findById(@Param('id') id: string){
+    return this.groupsService.findById(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateGroupDto: UpdateGroupDto) {
-    return this.groupsService.update(+id, updateGroupDto);
-  }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.groupsService.remove(+id);
-  }
+  
 }
--- splitwise-backend/src/groups/groups.service.ts
+++ splitwise-backend/src/groups/groups.service.ts
@@ -1,26 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Group } from './entities/group.entity';
 import { CreateGroupDto } from './dto/create-group.dto';
-import { UpdateGroupDto } from './dto/update-group.dto';
+import { UsersService } from '../users/users.service';
+
+
+
 
 @Injectable()
 export class GroupsService {
-  create(createGroupDto: CreateGroupDto) {
-    return 'This action adds a new group';
+  constructor(
+    @InjectRepository(Group)
+    private groupsRepository: Repository<Group>,
+    private usersService: UsersService,
+  ){}
+
+
+  // Create Group
+  async create(createGroupDto: CreateGroupDto, createdById: string){
+    const { name, memberIds } = createGroupDto;
+    const createdBy = await this.usersService.findById(createdById);
+    const members = await Promise.all(memberIds.map((id) => this.usersService.findById(id)))
+     const group = this.groupsRepository.create({
+         name,
+         createdBy,
+         members: [createdBy, ...members],
+       });
+    return this.groupsRepository.save(group);
   }
 
-  findAll() {
-    return `This action returns all groups`;
-  }
+  async findById(id: string) {
+    
+  const group = await this.groupsRepository.findOne({
+     where: { id },
+     relations: ['createdBy', 'members', 'expenses'],
+  })
+  
+  if (!group) throw new NotFoundException('Group not found');
 
-  findOne(id: number) {
-    return `This action returns a #${id} group`;
-  }
+  return group;
 
-  update(id: number, updateGroupDto: UpdateGroupDto) {
-    return `This action updates a #${id} group`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} group`;
   }
 }
